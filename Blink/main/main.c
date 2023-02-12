3335d4de1c97a2b502c5c1474d2eb5f4ca48f498
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-12 15:22:20 -0800

    Change the minimum configuration for the log messages

--- Blink/main/main.c
+++ Blink/main/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "esp_log.h"
 
 void app_main(void)
 {
+    //TAG
+    esp_log_level_set("LOG", ESP_LOG_INFO);
 
+    //Log messages.
+    ESP_LOGE("LOG", "This is an error!!! ");
+    ESP_LOGW("LOG", "This is a warning!!!");
+    ESP_LOGI("LOG","This is an information!!");
+    ESP_LOGD("LOG", "This is a debug message!!!");
+    ESP_LOGV("LOG","This is a verbose!!!");
+
+    int number = 0;
+    ESP_LOGE("ERROR", "This is an error!!! %d",number++);
+    ESP_LOGW("WARNING", "This is a warning!!! %d",number++);
+    ESP_LOGI("INfO","This is an information!! %d",number++);
+    ESP_LOGD("DEBUG", "This is a debug message!!! %d",number++);
+    ESP_LOGV("VERBOSE","This is a verbose!!! %d",number++);
 }
