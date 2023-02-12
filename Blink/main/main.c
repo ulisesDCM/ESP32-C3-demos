014ad5f69b1d160bd2f6e1c024e22a731fa370a4
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-12 15:38:45 -0800

    printing a simple message inside of the main loop with a 1 second delay

--- Blink/main/main.c
+++ Blink/main/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
@@ -19,4 +21,13 @@ void app_main(void)
     ESP_LOGI("INfO","This is an information!! %d",number++);
     ESP_LOGD("DEBUG", "This is a debug message!!! %d",number++);
     ESP_LOGV("VERBOSE","This is a verbose!!! %d",number++);
+
+    uint16_t loopCount = 0;
+
+    while (1)
+    {
+        ESP_LOGI("INF", "loopCount value is %d",loopCount++);
+        vTaskDelay(1000/portTICK_PERIOD_MS);                    //1 second delay
+    }
+    
 }
