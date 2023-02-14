9213835d71226532f1dafeff3e04bfe5e27cd380
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-13 16:25:50 -0800

    Finished input keyboard example

--- keyBoardInput/main/main.c
+++ keyBoardInput/main/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    char c = 0;
+    char str[100];
 
+    //Initialize the array with 0 values.
+    memset(str, 0, sizeof(str));
+
+    while(c != '\n')
+    {
+        c = getchar();
+        if(c != 0xff)
+        {
+            str[strlen(str)] = c;
+            printf("%c",c); 
+        }
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+
+    printf("You typed: %s\n",str);
 }
