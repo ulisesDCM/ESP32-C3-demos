1f76dc62874055ec741721b520366e7068a9728a
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-18 12:00:17 -0800

    Created two task as example.

--- basicFreeRTOS2/main/main.c
+++ basicFreeRTOS2/main/main.c
@@ -6,12 +6,46 @@
 
 #define ESP_LOG_TAG     "main.c"
 
-void app_main(void)
+void task1(void)
 {
+    while(1)
+    {
+        ESP_LOGI(ESP_LOG_TAG,"Running Task 1");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void task2(void)
+{
+    while (1)
+    {
+        ESP_LOGI(ESP_LOG_TAG, "Running Task 2");
+        vTaskDelay(pdMS_TO_TICKS(700));
+    }
+    
+}
+
+void app_main(void) 
+{
+    // Creating task 1.
+    xTaskCreate(&task1,
+                "Example Task 1",
+                2048,
+                NULL,
+                2,
+                NULL);
+    
+    // Creating task 2.
+    xTaskCreate(&task2,
+                "Example Task 2",
+                2048,
+                NULL,
+                2,
+                NULL);
 
     while(1)
     {
-        ESP_LOGI(ESP_LOG_TAG, "hello world!!!");
+        ESP_LOGI(ESP_LOG_TAG, "Running main loop");
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
