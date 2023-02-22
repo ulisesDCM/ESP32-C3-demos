6576727578a8ee0c5d6578ed09dce369791d2eeb
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-21 18:20:50 -0800

    Removed extra output configuration with interrupts.

--- gpio/main/main.c
+++ gpio/main/main.c
@@ -17,18 +17,13 @@
 #define GPIO1_INPUT_NINT_PIN    (1)
 #define GPIO0_1_MASK_BIT        ( (1<<GPIO0_INPUT_NINT_PIN) | (1<<GPIO1_INPUT_NINT_PIN))
 
-// Outputs with interrupt.  ??
-#define GPIO9_OUTPUT_INT_PIN    (9)
-#define GPIO8_OUTPUT_INT_PIN    (8)
-#define GPIO8_9_MASK_BIT        ( (1<<GPIO9_OUTPUT_INT_PIN) | (1<<GPIO8_OUTPUT_INT_PIN))
-
 // Inputs with interrupt.
 #define GPIO7_INPUT_INT_PIN     (7)
 #define GPIO6_INPUT_INT_PIN     (6)
 #define GPIO7_6_MASK_BIT        ( (1<<GPIO7_INPUT_INT_PIN) | (1<<GPIO6_INPUT_INT_PIN))
 
 
-static const gpio_config_t gpios_config_list[4] = 
+static const gpio_config_t gpios_config_list[] = 
 {   
     // Outputs without interrupt.
     {
@@ -42,15 +37,6 @@ static const gpio_config_t gpios_config_list[4] =
     // Inputs without interrupt.
     {
         .intr_type=GPIO_INTR_DISABLE,
-        .mode=GPIO_MODE_INPUT,
-        .pin_bit_mask=GPIO0_1_MASK_BIT,
-        .pull_down_en=GPIO_PULLDOWN_ENABLE,
-        .pull_up_en=GPIO_PULLUP_DISABLE
-    },
-
-    // Outputs with interrupt.  ??
-    {
-        // .intr_type=GPIO_INTR_,
         .mode=GPIO_MODE_INPUT,
         .pin_bit_mask=GPIO0_1_MASK_BIT,
         .pull_down_en=GPIO_PULLDOWN_ENABLE,
@@ -71,7 +57,7 @@ static const gpio_config_t gpios_config_list[4] =
 void app_main(void)
 {
     gpio_config(&gpios_config_list[0]);
-    
+
     while(1)
     {
         ESP_LOGI(MAIN_LOG_TAG, "Hello World!!!");
