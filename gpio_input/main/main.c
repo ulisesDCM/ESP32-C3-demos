ae75e85d34c7db766c3ac5de692412fe4f485cbf
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-21 17:47:40 -0800

    Added a general gpios project for demostration.

--- gpio_input/main/main.c
+++ gpio_input/main/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LOG_TAG     "main.c"
+
+
 void app_main(void)
 {
-
+    while(1)
+    {
+        ESP_LOGI(LOG_TAG, "Hello world!!!");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 }
