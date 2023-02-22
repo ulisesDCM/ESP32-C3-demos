a5205823786f157d8122598e6f5bbac488cbfa38
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-21 17:55:29 -0800

    Print hello world in the LOG console.

--- gpio/main/main.c
+++ gpio/main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#define MAIN_LOG_TAG    ("main.c")
+
 void app_main(void)
 {
-
+    while(1)
+    {
+        ESP_LOGI(MAIN_LOG_TAG, "Hello World!!!");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 }
