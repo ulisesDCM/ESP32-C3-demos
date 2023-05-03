f17af04905cbb98f9a468a44d243ca7c145e4249
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-02 19:51:53 -0700

    Added the "params" parameter in the task functions and passing a string to display.

--- basicFreeRTOS/components/tasks/tasks.c
+++ basicFreeRTOS/components/tasks/tasks.c
@@ -25,8 +25,45 @@
 
         use EVENT_GROUP if:
             Multiple things need to happen before I unlock a task.
+
+        FreeRTOS priority:
+            High number --> High priority
+            Low Number --> Low priority
 */
+
+static void task_example1_task1(void *params)
+{
+    while(1)
+    {
+        ESP_LOGI(LOG_TAG_TASKS,"%s",(char *) params);
+        vTaskDelay(pdMS_TO_TICKS(1000));  
+    }
+}
+
+static void task_example1_task2(void *params)
+{
+    while (1)
+    {
+        ESP_LOGI(LOG_TAG_TASKS, "%s",(char *) params);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 
 void task_example1(void)
 {
+    //Create task1
+    xTaskCreate(&task_example1_task1,
+                "task1",
+                1024*2,
+                "Running task1",
+                2,
+                NULL);
+
+    //Create task2
+    xTaskCreate(&task_example1_task2,
+                "task2",
+                1024*2,
+                "Running task2",
+                2,
+                NULL);
 }
--- basicFreeRTOS/main/main.c
+++ basicFreeRTOS/main/main.c
@@ -10,10 +10,10 @@
 
 void app_main(void) 
 {
+    task_example1();
     while(1)
-    {        
-        ESP_LOGI(ESP_LOG_TAG, "Running main loop");
-        task_example1();
-        vTaskDelay(pdMS_TO_TICKS(500));
+    {
+        ESP_LOGI(ESP_LOG_TAG, "Running main loop");        
+        vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
