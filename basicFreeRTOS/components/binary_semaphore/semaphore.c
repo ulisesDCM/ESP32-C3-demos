4672d5b6f00e27a6b3e8e95e15391e21d8db67d0
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-03 14:51:50 -0700

    Added the semaphore demostration code.

--- basicFreeRTOS/components/binary_semaphore/semaphore.c
+++ basicFreeRTOS/components/binary_semaphore/semaphore.c
@@ -8,12 +8,16 @@
 
 #define LOG_TAG_SEMAPHORE   ("semaphore.c")
 
+SemaphoreHandle_t binSemaphore;
+
 static void semaphore_task1(void *params)
 {
     while(1)
     {
         ESP_LOGI(LOG_TAG_SEMAPHORE, "Running semaphore task1.");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        xSemaphoreGive(binSemaphore);
+        ESP_LOGI(LOG_TAG_SEMAPHORE, "Process semaphore task1.");
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
 
@@ -21,16 +25,19 @@ static void semaphore_task2(void *params)
 {
     while(1)
     {
+        xSemaphoreTake(binSemaphore, portMAX_DELAY);    //Wait forever until semaphore is available.
         ESP_LOGI(LOG_TAG_SEMAPHORE, "Running semaphore task2.");
-        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void semaphore_example1(void)
 {
     ESP_LOGI(LOG_TAG_SEMAPHORE, "Running semaphore example 1, creating tasks.");
+
+    binSemaphore = xSemaphoreCreateBinary();        //Creating binary semaphore.
     xTaskCreate(&semaphore_task1, "semaphore task1", 1024*2, NULL, 2, NULL);
     xTaskCreate(&semaphore_task2, "semaphore task2", 1024*2, NULL, 2, NULL);
+
     ESP_LOGI(LOG_TAG_SEMAPHORE, "Finish to create tasks.");
 
 }
