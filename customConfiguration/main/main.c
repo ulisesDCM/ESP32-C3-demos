77e78577b2dfa609b9bb048eef7992f4ef499ea7
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-05 16:23:08 -0700

    Dispaly hello world message every 1 second

--- customConfiguration/main/main.c
+++ customConfiguration/main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LOG_TAG     ("main.c")
+
 void app_main(void)
 {
-
+    while(1)
+    {
+        ESP_LOGI(LOG_TAG, "Hello world!!!");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
