191a6818f6f45837209275af9ca822c7d1f332c9
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-05 16:40:51 -0700

    Displaying all custom configuration values.

--- customConfiguration/main/main.c
+++ customConfiguration/main/main.c
@@ -11,6 +11,21 @@ void app_main(void)
     while(1)
     {
         ESP_LOGI(LOG_TAG, "Hello world!!!");
+
+        #ifdef CONFIG_MY_BOOL
+            ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_MY_BOOL %d",CONFIG_MY_BOOL);
+        #endif
+        ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_MY_STRING %s",CONFIG_MY_STRING);
+        ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_MY_INT %d",CONFIG_MY_INT);
+
+        #ifdef CONFIG_OPTION_1
+            ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_OPTION_1 %d",CONFIG_OPTION_1);
+        #elif CONFIG_OPTION_2
+            ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_OPTION_2 %d",CONFIG_OPTION_2);
+        #elif CONFIG_OPTION_3
+            ESP_LOGI(LOG_TAG, "Custom menuconfig, CONFIG_OPTION_3 %d",CONFIG_OPTION_3);
+        #endif
+
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
