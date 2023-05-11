9c4724f855996b74ee597439a79c9b1c8d4966a4
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-11 12:21:50 -0700

    Added the definition of the interrupt pin and the includes for the gpio drivers.

--- gpio/components/gpioInterrupt/gpioInterrupt.c
+++ gpio/components/gpioInterrupt/gpioInterrupt.c
@@ -1,10 +1,14 @@
 #include "gpioInterrupt.h"
 
 #include "esp_log.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define LOG_TAG     ("gpioInterrupt.c")
 
 void gpio_int_config(void)
 {
+    
 }
 
--- gpio/components/gpioInterrupt/gpioInterrupt.h
+++ gpio/components/gpioInterrupt/gpioInterrupt.h
@@ -1,6 +1,9 @@
 #ifndef _GPIO_INTERRUPT_H_
 #define _GPIO_INTERRUPT_H_
 
+#define INPUT_INT_PIN   (6)
+#define INPUT_INT_BIT_MASK  (INPUT_INT_PIN<<1)
+
 void gpio_int_config(void);
 
 #endif
