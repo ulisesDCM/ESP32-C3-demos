7552ce212d04400838bf74adcc6dfc07d3476620
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-11 12:26:00 -0700

    Added the gpio configuration for the interrupt.

--- gpio/components/gpioInterrupt/gpioInterrupt.c
+++ gpio/components/gpioInterrupt/gpioInterrupt.c
@@ -7,8 +7,17 @@
 
 #define LOG_TAG     ("gpioInterrupt.c")
 
+const gpio_config_t int_config = {
+    .pin_bit_mask=INPUT_INT_BIT_MASK,
+    .mode=GPIO_MODE_INPUT,
+    .pull_up_en=GPIO_PULLUP_DISABLE,
+    .pull_down_en=GPIO_PULLDOWN_ENABLE,
+    .intr_type=GPIO_INTR_POSEDGE
+};
+
 void gpio_int_config(void)
 {
+    ESP_LOGI(LOG_TAG, "Initialize the %d pin to be an interrupt",INPUT_INT_PIN);
     
 }
 
