6f080b6f37bb757a75551b26346e8c986d3f6e74
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-12 14:29:34 -0700

    Finish to configure the timer and channel for the PWM output

--- PWM/main/PWM/pwm.c
+++ PWM/main/PWM/pwm.c
@@ -7,5 +7,25 @@
 
 void init_pwm_led(void)
 {
-    
+    ledc_timer_config_t timer_conf = {
+        .speed_mode=LEDC_LOW_SPEED_MODE,
+        .duty_resolution=LEDC_TIMER_10_BIT,
+        .timer_num=LEDC_TIMER_0,
+        .freq_hz=5000,
+        .clk_cfg=LEDC_AUTO_CLK
+    };
+
+    ledc_channel_config_t channel_conf = {
+        .gpio_num=5,
+        .speed_mode=LEDC_LOW_SPEED_MODE,
+        .channel=LEDC_CHANNEL_0,
+        .intr_type=LEDC_INTR_DISABLE,
+        .timer_sel=LEDC_TIMER_0,
+        .duty=10,         //[0, (2**duty_resolution)] 
+        .hpoint=0
+    };
+
+    ledc_timer_config(&timer_conf);
+    ledc_channel_config(&channel_conf);    
+
 }
--- PWM/main/main.c
+++ PWM/main/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "pwm.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    init_pwm_led();
     while (1)
     {
+        printf("helloWorld!\r\n");
+        vTaskDelay(1000/portTICK_PERIOD_MS);        
     }
     
 }
