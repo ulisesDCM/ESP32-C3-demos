e15746df23b2edd19997d90e565965b989d00bf8
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-12 11:50:09 -0700

    Added new demo project to configure and use the DAC peripheral

---
+++ DAC/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LOG_TAG ("main.c")
+
+void app_main(void)
+{
+    while (1)
+    {
+        ESP_LOGI(LOG_TAG, "Hello World");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
