8e40a33cc825a5df39b73043a14b58a4087f013d
ulisesDCM <ulises.cmorales@hotmail.com>
2023-05-12 12:11:43 -0700

    Added the basic instructions to control the DAC channel 1. Because in the esp32c3 we don't have any available DAC, we can't test the demo, just using a compatible ESP32 version such as ESP32-WROOM-32

--- DAC/main/DAC/dac.c
+++ DAC/main/DAC/dac.c
@@ -4,9 +4,19 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "driver/gpio.h"
+#include "driver/dac_common.h"
+
 #define LOG_TAG ("dac.c")
+
+/* 
+    255 = 3.3v
+    Resolution  8-bits 
+    Maximum voltage 3.3V
+*/
 
 void init_dac(void)
 {
-    
+    dac_output_enable(DAC_CHANNEL_1);
+    dac_output_voltage(DAC_CHANNEL_1, 255 );        
 }
