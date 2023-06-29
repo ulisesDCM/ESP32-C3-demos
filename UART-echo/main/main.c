552d065c1f682500f0d68e3faa0b48f5f3ef948a
ulisesDCM <ulises.cmorales@hotmail.com>
2023-06-29 16:14:25 -0700

    Added basic echo program using UART

---
+++ UART-echo/main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/uart.h"
+#include <string.h>
+
+#define TXD_PIN 4
+#define RXD_PIN 5
+
+#define RX_BUF_SIZE     254
+
+void app_main(void)
+{
+
+}
