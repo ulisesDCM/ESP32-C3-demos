35728ed2657d6d561a0f28f73ddb23ce4a125a3c
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-04 16:21:14 -0700

    Added i2c headers and basic definition for I2C pins and slave address

--- i2c-LM75A/main/main.c
+++ i2c-LM75A/main/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c.h"
+#include "esp_types.h"
+
+#define SDA_GPIO        26
+#define SCL_GPIO        25
+#define LM75A_ADDRESS   0x48
 
 void app_main(void)
 {
