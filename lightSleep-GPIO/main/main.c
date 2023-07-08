48344f4426c75c384f8c47f0d98bc67e8055fe38
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-08 06:20:20 -0700

    Finish basic demostration on how to use a gpio input pin to wake up the ESP32 from light sleep mode.

--- lightSleep-GPIO/main/main.c
+++ lightSleep-GPIO/main/main.c
@@ -9,16 +9,39 @@
 #include "esp_log.h"
 #include "esp_timer.h"
 // #include "esp32/rom/uart.h"
+#include "driver/rtc_io.h"
+#include "esp_rom_gpio.h"
+
+#define INPUT_PIN   0
 
 void app_main(void)
 {
+    esp_rom_gpio_pad_select_gpio(INPUT_PIN);
+    gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+    gpio_wakeup_enable(INPUT_PIN, GPIO_INTR_LOW_LEVEL);
+
+    esp_sleep_enable_gpio_wakeup();
     esp_sleep_enable_timer_wakeup(5*1000000);
-    printf("going for a nap\n");
-    vTaskDelay(pdMS_TO_TICKS(100));  //Just to wait until printf send the message to the console.
 
-    int64_t before = esp_timer_get_time();
-    esp_light_sleep_start();
-    int64_t after = esp_timer_get_time();
+    while(true){
 
-    printf("napped for %lld\n",(after-before)/1000);
+        if(rtc_gpio_get_level(INPUT_PIN)==0){
+            printf("please release button...\n");
+            do{
+                vTaskDelay(pdMS_TO_TICKS(10));
+            }while(rtc_gpio_get_level(INPUT_PIN)==0);
+        }
+
+        printf("going for a nap\n");
+        vTaskDelay(pdMS_TO_TICKS(100));  //Just to wait until printf send the message to the console.
+
+        int64_t before = esp_timer_get_time();
+        esp_light_sleep_start();
+        int64_t after = esp_timer_get_time();
+
+        esp_sleep_wakeup_cause_t reason = esp_sleep_get_wakeup_cause();
+
+        printf("napped for %lld, reason was: %s\n",(after-before)/1000,
+                    reason == ESP_SLEEP_WAKEUP_TIMER? "timer": "button");
+    }
 }
