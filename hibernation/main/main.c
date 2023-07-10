88c22d67d3a4bea5dc5c3f1bde37c59ae9793951
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-10 06:44:51 -0700

    Finish to write demo project about hibernation mode.

--- hibernation/main/main.c
+++ hibernation/main/main.c
@@ -2,31 +2,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_sleep.h"
-#include "driver/rtc_io.h"
-
-#define BUTTON_1  GPIO_NUM_10
-#define BUTTON_2  GPIO_NUM_11
 
 /* Use the RTC_DATA_ATTR to store a variable/data in 
 a certaing part of the memory where is not deleted once the ESP32
 goes to the deep sleep mode.  */
-
 RTC_DATA_ATTR int timesWokenUp =0;  //<-- This variable won't be reseted 
 
 void app_main(void)
 {
-    rtc_gpio_deinit(BUTTON_1);
-    rtc_gpio_deinit(BUTTON_2);
-    //Genral gpio function using the pin.
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
     
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON); 
-    rtc_gpio_pullup_en(BUTTON_1);
-    rtc_gpio_pulldown_dis(BUTTON_1);
-    rtc_gpio_pullup_en(BUTTON_2);
-    rtc_gpio_pulldown_dis(BUTTON_2);
-
-    uint64_t buttons_mask = (1UL<<BUTTON_1) | (1UL<<BUTTON_2);
-    esp_sleep_enable_ext1_wakeup(buttons_mask, ESP_EXT1_WAKEUP_ALL_LOW);
+    /* 
+        To avoid the variable 'timesWokenUp' been corrupted you should avoid 
+        turning OFF the RTC_SLOW MEM.
+     */   
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_enable_timer_wakeup(5*1000000);
     printf("timesWokenUp = %d\n",timesWokenUp++);
     printf("going to deep sleep\n");
 
