b4bc4334f041352f162cd1f798622f210a5dcfa5
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-11 14:26:43 -0700

    Added the client request to google.com

--- wifi-example-connection/main/main.c
+++ wifi-example-connection/main/main.c
@@ -5,10 +5,33 @@
 #include "esp_http_client.h"
 #include "protocol_examples_common.h"
 
+esp_err_t clientEvent(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ON_DATA:
+        printf("%.*s\n",evt->data_len, (char *) evt->data);
+        break;
+    
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
 void app_main(void)
 {
     nvs_flash_init();
     esp_netif_init();
     esp_event_loop_create_default();
     example_connect();  
+
+    esp_http_client_config_t clientConfig ={
+        .url="http://google.com",
+        .event_handler=clientEvent
+    };
+
+    esp_http_client_handle_t client = esp_http_client_init(&clientConfig);
+    esp_http_client_perform(client);
+    esp_http_client_cleanup(client);
 }
