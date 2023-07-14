c8aa47e8c2c1c1f31a213781b78e90c630a2807c
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-13 19:44:52 -0700

    Updating files.

--- wifi-connection-lib/components/wifi/connect.c
+++ wifi-connection-lib/components/wifi/connect.c
@@ -8,11 +8,11 @@
 #define CONNECT_LOG_TAG     ("connect.c")
 
 void wifi_init(void){
-
 }
 
 esp_err_t wifi_connect_sta(const char *ssid, const char *pass, int timeout){
 
+    return  ESP_OK; 
 }
 
 void wifi_connect_ap(const char *ssid, const char *pass){
