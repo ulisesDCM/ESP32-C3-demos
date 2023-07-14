3ada4eca3f75c515b64e6bdae46929d3fd0ae455
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-13 21:17:25 -0700

    Added the event loop and the handler for the differents wifi initialization events.

--- wifi-connection-lib/components/wifi/connect.c
+++ wifi-connection-lib/components/wifi/connect.c
@@ -4,10 +4,45 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
 
 #define CONNECT_LOG_TAG     ("connect.c")
 
+void event_handler(void* event_handler_arg, esp_event_base_t event_base, int32_t event_id, 
+                    void* event_data){
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        ESP_LOGI(CONNECT_LOG_TAG, "connecting...");
+        esp_wifi_connect();
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        ESP_LOGI(CONNECT_LOG_TAG, "connected...");
+        break;
+
+    case WIFI_EVENT_STA_DISCONNECTED:
+        ESP_LOGI(CONNECT_LOG_TAG, "disconnected...");
+        break;
+
+    case IP_EVENT_STA_GOT_IP:
+        ESP_LOGI(CONNECT_LOG_TAG, "got ip");
+        break;
+    
+    default:
+        break;
+    }
+}
+
 void wifi_init(void){
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, event_handler, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
 }
 
 esp_err_t wifi_connect_sta(const char *ssid, const char *pass, int timeout){
--- wifi-connection-lib/main/main.c
+++ wifi-connection-lib/main/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "connect.h"
+#include "nvs_flash.h"
 
 void app_main(void)
 {
+    ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init();
-    
+
 }
