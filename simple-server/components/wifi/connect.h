2ad75a4f7291c0f0489dc12826994029fa104b3d
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-19 15:27:05 -0700

    Added new project that creates a simple web server in the ESP32

---
+++ simple-server/components/wifi/connect.h
@@ -0,0 +1,11 @@
+#ifndef __CONNECT_H_
+#define __CONNECT_H_
+
+#include "esp_err.h"
+
+void wifi_init(void);
+esp_err_t wifi_connect_sta(const char *ssid, const char *pass, int timeout);
+void wifi_connect_ap(const char *ssid, const char *pass);
+void wifi_disconnect(void);
+
+#endif
---
+++ simple-server/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "connect.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+#include "esp_http_server.h"
+
+#define LOG_TAG     ("main.c")
+
+void app_main(void)
+{
+    ESP_LOGI(LOG_TAG,"Hello world!!!");
+}
