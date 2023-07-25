b6f020d21681e1eb5469c03485231e215f49d006
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-25 06:02:50 -0700

    Added the push button gpio configuration.

--- simple-server/main/pushButton.c
+++ simple-server/main/pushButton.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "pushButton.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define LOG_TAG             ("pushbutton.c")
+#define PUSH_BUTTON_PIN     (GPIO_NUM_3)
+#define PUSH_BUTTON_MASK    (1<<PUSH_BUTTON_PIN)
+
+const gpio_config_t button_config = {
+    .pin_bit_mask=PUSH_BUTTON_MASK,
+    .mode=GPIO_MODE_INPUT,
+    .pull_up_en=GPIO_PULLUP_ENABLE,
+    .pull_down_en=GPIO_PULLDOWN_DISABLE,
+    .intr_type=GPIO_INTR_DISABLE
+};
+
+void init_button(void){
+    ESP_ERROR_CHECK(gpio_reset_pin(PUSH_BUTTON_PIN));
+    ESP_ERROR_CHECK(gpio_config(&button_config));
+}
--- simple-server/main/pushButton.h
+++ simple-server/main/pushButton.h
@@ -1,4 +1,6 @@
 #ifndef __PUSHBUTTON_H_
 #define __PUSHBUTTON_H_
 
+void init_button(void);
+
 #endif
