d7c577c6436785e1ad25a83599fd8223b1b714f0
ulisesDCM <ulises.cmorales@hotmail.com>
2023-07-25 06:31:18 -0700

    Creating the web socket

--- simple-server/main/main.c
+++ simple-server/main/main.c
@@ -36,11 +36,16 @@ static  esp_err_t on_toogle_led(httpd_req_t *r){
     return ESP_OK;
 }
 
+static  esp_err_t on_web_socket_url(httpd_req_t *r){
+    return ESP_OK;
+}
+
 static void init_server(void){
     httpd_handle_t server=NULL;
     httpd_config_t config=HTTPD_DEFAULT_CONFIG();
     ESP_ERROR_CHECK(httpd_start(&server, &config));
 
+    /* Defining default URL to publish a hello worl message using GET */
     httpd_uri_t default_url={
         .uri="/",
         .method=HTTP_GET,
@@ -48,6 +53,7 @@ static void init_server(void){
     };
     httpd_register_uri_handler(server, &default_url);
 
+    /* Defining default URL to toogle an LED using POST */
     httpd_uri_t toogle_led_url={
         .uri="/api/toogle-led",
         .method=HTTP_POST,
@@ -55,6 +61,17 @@ static void init_server(void){
     };
 
     httpd_register_uri_handler(server, &toogle_led_url);
+
+    /* Defining default URL for the web socket using GET */
+    httpd_uri_t web_socket_url={
+        .uri="/ws",
+        .method=HTTP_GET,
+        .handler=on_web_socket_url,
+        .is_websocket=true
+    };
+
+    httpd_register_uri_handler(server, &web_socket_url);
+
 }
 
 void start_mdns_server(void){
