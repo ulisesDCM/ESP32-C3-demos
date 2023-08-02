8fec201f3d53255830faa7050e1a09ccf39bdecd
ulisesDCM <ulises.cmorales@hotmail.com>
2023-08-02 06:58:11 -0700

    Added the parititon table to allocated the website on the ESP32

--- website-on-chip/site/svelte.config.js
+++ website-on-chip/site/svelte.config.js
@@ -3,17 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 export default {
     kit: {
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        }) 
+            fallback: '200.html' // may differ from host to host
+        })
     }
 };
-
 // import adapter from '@sveltejs/adapter-auto';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 // /** @type {import('@sveltejs/kit').Config} */
